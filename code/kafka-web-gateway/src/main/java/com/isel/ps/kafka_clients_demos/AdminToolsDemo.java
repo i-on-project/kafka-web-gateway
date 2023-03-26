b28d1a5e61e1957e4c29bdc132acafb8550b0bc4
mjbmarques <mjbmarques@live.com.pt>
2023-03-26 14:58:45 +0100

    Updating Admin tools as it had an error in demo and updated graddle and gitignore.

--- code/kafka-web-gateway/src/main/java/com/isel/ps/kafka_clients_demos/AdminToolsDemo.java
+++ code/kafka-web-gateway/src/main/java/com/isel/ps/kafka_clients_demos/AdminToolsDemo.java
@@ -2,14 +2,19 @@ package com.isel.ps.kafka_clients_demos;
 
 import com.isel.ps.kafka_clients.AdminTools;
 
+import java.util.Scanner;
 import java.util.concurrent.ExecutionException;
 
 public class AdminToolsDemo {
     //private static final Logger log = LogManager.getLogger(ProducerDemo.class);
+    private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         AdminTools admin = new AdminTools();
-        admin.createTopic("demo-topic", 5, (short) 3);
+        System.out.println("Started Administration Tools");
+        System.out.print("Input the topic name you want to create:");
+        String topicname = scanner.nextLine();
+        System.out.println(admin.createTopic(topicname, 5, (short) 1).get().toString());
         System.out.println("Existing topics names:");
         for (String topic : admin.getAllTopics().get()) {
             System.out.println(String.format("topic: [%s]", topic));
