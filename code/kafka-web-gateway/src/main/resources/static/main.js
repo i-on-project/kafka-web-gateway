e6d888eae099bf5c22bbd7f37a20c38dedf3abe7
Alexandre Silva <alexandre2000@live.com.pt>
2023-05-26 00:32:36 +0100

    Started front end implementation

--- code/kafka-web-gateway/src/main/resources/static/main.js
+++ code/kafka-web-gateway/src/main/resources/static/main.js
@@ -1,87 +1,118 @@
-let ws;
+let outputElement
+let subscribeTopicsInput
+let consumeQuantityInput
+let consumeScaleInput
+let publishTopicInput
+let publishKeyInput
+let publishValueInput
+let pauseTopicsInput
+let resumeTopicsInput
 
-function setConnectionState(connected) {
-    document.getElementById("connect").disabled = connected;
-    document.getElementById("disconnect").disabled = !connected;
-    document.getElementById("subscribe").disabled = !connected;
-    document.getElementById("produce").disabled = !connected;
-
-    if (connected) {
-        document.getElementById("conversation").style.display = "block";
-    } else {
-        document.getElementById("conversation").style.display = "none";
-    }
-    document.getElementById("records").innerHTML = "";
-}
-
-function connect() {
-    ws = new WebSocket(document.getElementById("url").value);
-    ws.onopen = function () {
-        setConnectionState(true);
-    }
-    ws.onmessage = function (data) {
-        showRecord(data.data);
-    };
-    ws.onclose = function () {
-        setConnectionState(false);
-    }
-}
-
-function disconnect() {
-    if (ws != null) {
-        ws.close();
-    }
-    setConnectionState(false);
-    console.log("Disconnected");
-}
-
-function sendName() {
-    const data = JSON.stringify({'name': document.getElementById("name").value});
-    ws.send(data);
-}
-
-// TODO: change data sent to accept offset too
-function sendSubscribe() {
-    const topics = document.getElementById("subscribe-topic").value;
-    const data = `subscribe:${topics}`;
-    ws.send(data);
-}
-
-function produceRecord() {
-    const topic = document.getElementById("produce-topic").value;
-    const key = document.getElementById("produce-key").value;
-    const value = document.getElementById("produce-value").value;
-    const data = `publish:${topic},${key},${value}`;
-    ws.send(data);
-}
-
-function showRecord(message) {
-    const greetings = document.getElementById("records");
-    const tr = document.createElement("tr");
-    const td = document.createElement("td");
-    td.innerHTML = message;
-    tr.appendChild(td);
-    greetings.appendChild(tr);
-}
-
-window.addEventListener('DOMContentLoaded', function () {
-    document.getElementById("conversation").style.display = "none";
-    document.getElementById("subscribe").disabled = true;
-    document.getElementById("produce").disabled = true;
-    document.querySelector("form").addEventListener('submit', function (e) {
-        e.preventDefault();
-    });
-    document.getElementById("connect").addEventListener('click', function () {
-        connect();
-    });
-    document.getElementById("disconnect").addEventListener('click', function () {
-        disconnect();
-    });
-    document.getElementById("subscribe").addEventListener('click', function () {
-        sendSubscribe();
-    });
-    document.getElementById("produce").addEventListener('click', function () {
-        produceRecord();
-    });
+document.addEventListener('DOMContentLoaded', function () {
+    outputElement = document.getElementById('output');
+    subscribeTopicsInput = document.getElementById('subscribeTopics');
+    consumeQuantityInput = document.getElementById('consumeQuantity');
+    consumeScaleInput = document.getElementById('consumeScale');
+    publishTopicInput = document.getElementById('publishTopic');
+    publishKeyInput = document.getElementById('publishKey');
+    publishValueInput = document.getElementById('publishValue');
+    pauseTopicsInput = document.getElementById('pauseTopics');
+    resumeTopicsInput = document.getElementById('resumeTopics');
 });
 
+const webSocket = new WebSocket('ws://localhost:8080/socket?token=teste');
+
+webSocket.onopen = function () {
+    console.log('WebSocket connection established.');
+};
+
+webSocket.onmessage = function (event) {
+    const message = JSON.parse(event.data);
+    console.log('Received message:', message);
+
+    const listItem = document.createElement('li');
+    listItem.classList.add('list-group-item');
+    listItem.textContent = JSON.stringify(message);
+    outputElement.appendChild(listItem);
+};
+
+function subscribe() {
+    const topics = subscribeTopicsInput.value.split(',').map(topic => topic.trim());
+    const command = {
+        messageID: generateMessageID(),
+        command: {
+            type: 'subscribe',
+            topics: topics,
+            startConsuming: false
+        }
+    };
+
+    webSocket.send(JSON.stringify(command));
+    console.log('Sent command:', command);
+}
+
+function consume() {
+    const maxQuantity = parseInt(consumeQuantityInput.value);
+    const scale = consumeScaleInput.value;
+    const command = {
+        messageID: generateMessageID(),
+        command: {
+            type: 'consume',
+            maxQuantity: maxQuantity,
+            scale: scale
+        }
+    };
+
+    webSocket.send(JSON.stringify(command));
+    console.log('Sent command:', command);
+}
+
+function publish() {
+    const topic = publishTopicInput.value;
+    const key = publishKeyInput.value;
+    const value = publishValueInput.value;
+    const command = {
+        messageID: generateMessageID(),
+        command: {
+            type: 'publish',
+            topic: topic,
+            key: key,
+            value: value
+        }
+    };
+
+    webSocket.send(JSON.stringify(command));
+    console.log('Sent command:', command);
+}
+
+function pause() {
+    const topics = pauseTopicsInput.value.split(',').map(topic => topic.trim());
+    const command = {
+        messageID: generateMessageID(),
+        command: {
+            type: 'pause',
+            topics: topics
+        }
+    };
+
+    webSocket.send(JSON.stringify(command));
+    console.log('Sent command:', command);
+}
+
+function resume() {
+    const topics = resumeTopicsInput.value.split(',').map(topic => topic.trim());
+    const command = {
+        messageID: generateMessageID(),
+        command: {
+            type: 'resume',
+            topics: topics
+        }
+    };
+
+    webSocket.send(JSON.stringify(command));
+    console.log('Sent command:', command);
+}
+
+function generateMessageID() {
+    return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+}
